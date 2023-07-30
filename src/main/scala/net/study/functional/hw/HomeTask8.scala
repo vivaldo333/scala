f2f3925cd8762508e4e32ab050e06b0464c40a6d
Volodymyr Tsymbrovskyi <VOLODYMYR.TSYMBROVSK@kyivstar.net>
2023-07-30 22:21:43 +0300

    feat(HW-8): Refactor task with PartialFunction

--- src/main/scala/net/study/functional/hw/HomeTask8.scala
+++ src/main/scala/net/study/functional/hw/HomeTask8.scala
@@ -1,10 +1,6 @@
 package net.study.functional.hw
 
-import net.study.functional.hw.HomeTask8.Product
-
 import scala.annotation.tailrec
-import scala.collection.immutable.Set
-import scala.collection.mutable.ListBuffer
 import scala.util.Random
 
 object HomeTask8 extends App {
@@ -72,7 +68,6 @@ object HomeTask8 extends App {
       case c if c >= 300 => PaymentResponse(500, geographicalPriority)
       case c if c % 2 == 0 => PaymentResponse(200, geographicalPriority)
       case c if c % 2 != 0 => PaymentResponse(400, geographicalPriority)
-
     }
 
     protected val products: Map[ProductType, Int] = Map(CustomerInsurance -> 1, LifeInsurance -> 2, CreditPayment -> 3, Charity -> 4)
@@ -99,7 +94,7 @@ object HomeTask8 extends App {
 
     override def canProcess[T <: ProductType](product: T): Boolean = product match {
       case CreditCardPayment => false
-      case _                 => products.get(product).exists(_ <= geographicalPriority)
+      case _ => products.get(product).exists(_ <= geographicalPriority)
     }
   }))
 
@@ -137,7 +132,7 @@ object HomeTask8 extends App {
   def chainDomains[IN, OUT](domains: List[BusinessDomain[IN, OUT]], default: BusinessDomain[IN, OUT]): BusinessDomain[IN, OUT] = {
     @tailrec
     def chain(domains: List[BusinessDomain[IN, OUT]], acc: BusinessDomain[IN, OUT]): BusinessDomain[IN, OUT] = domains match {
-      case Nil          => acc
+      case Nil => acc
       case head :: tail => chain(tail, acc orElse head)
     }
 
@@ -184,8 +179,13 @@ object HomeTask8 extends App {
 
   /////////////////////////////Implementation///////////////////////////////////////
 
+  // declare types
+  /*type PrePaymentDomain = PartialFunction[PaymentRequest, PaymentEnrichRequest]
+
+  type AlternativePaymentDomain = PartialFunction[PaymentEnrichRequest, PaymentResult]*/
+
   // implementation PostPaymentService
-  def getStatusCodes(status: Status): Set[Int] = statusCodes.getOrElse(Processed, Set.empty)
+  /*def getStatusCodes(status: Status): Set[Int] = statusCodes.getOrElse(Processed, Set.empty)
 
   val processedPostPaymentService = new PostPaymentService {
     override def serviceStatus: Status = Processed
@@ -209,7 +209,7 @@ object HomeTask8 extends App {
     override def serviceStatus: Status = Default
 
     override def codesToProcess: Set[Int] = Set.empty
-  }
+  }*/
 
   //--
   //   PaymentRequest ->
@@ -217,19 +217,58 @@ object HomeTask8 extends App {
   //   PartialFunction[PaymentResult, PaymentStatus] ->
   //   PaymentStatus
 
+  //Test on PartialFunction
+  //paymentDomain
+  //postPaymentDomain
+  //paymentFlow
 
-  def generatePaymentTokenMixParam(msisdn: String, tempCode: Int, product: Product): PaymentToken = SecurityServer.generatePaymentToken(msisdn, product, tempCode)
+  payments
+  val sortedPaymentDomains = serverPool.sortBy(_.geographicalPriority) map createPaymentDomain toList
+  val defaultPaymentDomain: PaymentDomain = {
+    case req => PaymentResult(req, PaymentResponse(-1, -1))
+  }
+
+  val paymentDomain = chainDomains(sortedPaymentDomains, defaultPaymentDomain)
+
+  def createPaymentDomain(server: PaymentService) : PaymentDomain = {
+    case req if server.canProcess(req.productType) =>
+      val tokenProvider: TokenProvider = SecurityServer.generatePaymentToken(req.msisdn, _:ProductType, req.tempCode)
+      val paymentResponse = server.withdrawPayment(req, tokenProvider)
+      PaymentResult(req, paymentResponse)
+  }
+
+  val postPaymentServices = statusCodes map {postPaymentCodeTuple =>
+    val (status, codes) = postPaymentCodeTuple
+    new PostPaymentService {
+      override def serviceStatus: Status = status
+      override def codesToProcess: Set[Int] = codes
+    }
+  } toList
+  val defaultPostPaymentDomain: PostPaymentDomain = {
+    case result => PaymentStatus(result.request, Default)
+  }
+  val postPaymentDomain: PostPaymentDomain = chainDomains(postPaymentServices map createPostPaymentDomain, defaultPostPaymentDomain)
+
+  def createPostPaymentDomain(postPaymentService: PostPaymentService): PostPaymentDomain = {
+    case paymentResult if postPaymentService.codesToProcess.contains(paymentResult.response.code) =>
+      // here will be any complicated business logic for this status - for example, analytic, brokers
+      PaymentStatus(paymentResult.request, postPaymentService.serviceStatus)
+  }
+
+  val paymentFlow = paymentDomain andThen postPaymentDomain
+
+  val paymentStatuses = payments map paymentFlow
+
+  paymentStatuses.foreach(println)
+
+
+  //-- Token
+  /*def generatePaymentTokenMixParam(msisdn: String, tempCode: Int, product: Product): PaymentToken =
+    SecurityServer.generatePaymentToken(msisdn, product, tempCode)
 
   val tokenProviderWithAllParam: String => Int => Product => PaymentToken = (generatePaymentTokenMixParam _).curried
 
-  var processedPayments = new ListBuffer[PaymentResult]()
-
-  serverPool.foreach(server => {
-    payments.filter(payment => server.canProcess(payment.productType))
-      .map(payment => PaymentResult(payment, server.withdrawPayment(payment, tokenProviderWithAllParam(payment.msisdn)(payment.tempCode))))
-      .foreach(result => processedPayments += result)
-  })
-
+  // prepare Enum
   val postServiceByCode: Map[Int, PostPaymentService] = Map(
     200 -> processedPostPaymentService,
     201 -> processedPostPaymentService,
@@ -245,14 +284,51 @@ object HomeTask8 extends App {
     503 -> serverMistakePostPaymentService
   )
 
-  val paymentStatus: PostPaymentDomain = {
+  //--
+  case class PaymentEnrichRequest(req: PaymentRequest, server: Option[PaymentService])
+
+  def canProcessPredicate(productType: ProductType): PaymentService => Boolean =
+    service => service.canProcess(productType)
+
+  val paymentEnrichRequestFunc: PrePaymentDomain = {
+    case req if serverPool.exists(canProcessPredicate(req.productType)) =>
+      val servers = serverPool.takeWhile(canProcessPredicate(req.productType))
+      val server = if (servers.isEmpty) None else Some(servers.head)
+      PaymentEnrichRequest(req, server)
+  }
+
+  val prePaymentDomainFunc: AlternativePaymentDomain = new AlternativePaymentDomain {
+    override def isDefinedAt(enrichReq: PaymentEnrichRequest): Boolean =
+      enrichReq.server.map(server =>
+        server.withdrawPayment(enrichReq.req, tokenProviderWithAllParam(enrichReq.req.msisdn)(enrichReq.req.tempCode))).isDefined
+
+    override def apply(enrichReq: PaymentEnrichRequest): PaymentResult = {
+      val response = enrichReq.server match {
+        case Some(server) => server.withdrawPayment(enrichReq.req, tokenProviderWithAllParam(enrichReq.req.msisdn)(enrichReq.req.tempCode))
+        case _ => PaymentResponse(500, 1)
+      }
+      PaymentResult(enrichReq.req, response)
+    }
+  }*/
+  /*case enrichReq =>
+    val paymentResponse = enrichReq.server.map(server =>
+        server.withdrawPayment(enrichReq.req, tokenProviderWithAllParam(enrichReq.req.msisdn)(enrichReq.req.tempCode)))
+      .getOrElse(PaymentResponse(-1, -1))
+    PaymentResult(enrichReq.req, paymentResponse)*/
+
+  /*val postPaymentDomainFunc: PostPaymentDomain = {
     case result if postServiceByCode.isDefinedAt(result.response.code) =>
       val status = postServiceByCode.getOrElse(result.response.code, defaultPostPaymentService).processResult(result)
       PaymentStatus(result.request, status)
   }
 
-  val result: Seq[PaymentStatus] = processedPayments map {
-    paymentStatus
-  }
-  println(result)
+  val result: Seq[PaymentStatus] = payments.map(payment =>
+    postPaymentDomainFunc(
+      prePaymentDomainFunc(
+        paymentEnrichRequestFunc(payment)
+      )
+    )
+  )
+
+  println(result)*/
 }
